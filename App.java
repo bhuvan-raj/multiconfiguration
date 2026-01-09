1e694b485ed11160c66276556955c86888dc198b
Bhuvan Raj <163994129+bhuvan-raj@users.noreply.github.com>
2026-01-09 10:16:12 +0530

    Create App.java

---
+++ App.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from GitHub Actions Matrix Build!");
+        System.out.println("Java version: " + System.getProperty("java.version"));
+        System.out.println("OS: " + System.getProperty("os.name"));
+    }
+}
